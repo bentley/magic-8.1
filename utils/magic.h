2ddabe37c26fce32fff20607ba5d0441a8e34d4a
Anthony J. Bentley <anthony@cathet.us>
2014-10-24 22:55:48 -0600

    Replace complicated dlong definition with int64_t.

--- utils/magic.h
+++ utils/magic.h
@@ -23,6 +23,7 @@
 
 #include <errno.h>
 #include <limits.h>
+#include <stdint.h>
 
 /* ------------------- Universal pointer typecast --------------------- */
 
@@ -41,63 +42,10 @@ typedef signed long spointertype;
 ERROR: Cannot compile without knowing the size of a pointer.  See utils/magic.h
 #endif
 
-/*-------------------------------------------------------------------
- * Define a double integer.  This replaces the horrible mess that
- * used to be the "DoubleInt" module.  Use of dlong should
- * improve the portability of magic.
- *
- * This defines "dlong" to be an 8-byte double-precision integer.
- *-------------------------------------------------------------------
- */
-
-#ifdef HAVE_LIMITS_H
-#include <limits.h>
-#else
-  #ifdef HAVE_SYS_PARAM_H
-  #include <sys/param.h>
-  #endif
-#endif
-
-#if defined(LONG_MAX) && defined(INT_MAX) 
-  #if LONG_MAX == INT_MAX
-  typedef long long dlong;
-  #define DLONG_PREFIX "ll"
-  #else
-  typedef long dlong;
-  #define DLONG_PREFIX "l"
-  #endif
-#else
-typedef long long dlong;
+typedef int64_t dlong;
+#define DLONG_MAX INT64_MAX
+#define DLONG_MIN INT64_MIN
 #define DLONG_PREFIX "ll"
-#endif
-
-/* Modified by NP & Tim 7/04 */
-// MAX vaule of dlong = 9223372036854775807LL [2^63]
-#ifndef DLONG_MAX
-  /* Note:  Linux defines LLONG_MAX but not LONG_LONG_MAX */
-  #ifdef LLONG_MAX
-  #define DLONG_MAX LLONG_MAX
-  #else
-    #ifdef LONG_LONG_MAX
-    #define DLONG_MAX LONG_LONG_MAX
-    #else
-    #define DLONG_MAX 0x7FFFFFFFFFFFFFFFLL
-    #endif
-  #endif
-#endif
-
-#ifndef DLONG_MIN
-  /* Note:  Linux defines LLONG_MIN but not LONG_LONG_MIN */
-  #ifdef LLONG_MIN
-  #define DLONG_MIN LLONG_MIN
-  #else
-    #ifdef LONG_LONG_MIN
-    #define DLONG_MIN LONG_LONG_MIN
-    #else
-    #define DLONG_MIN 0x8000000000000000LL
-    #endif
-  #endif
-#endif
 
 /* --------------------- Universal pointer type ----------------------- */
 
